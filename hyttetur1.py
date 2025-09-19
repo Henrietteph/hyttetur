80ebd9e932b83cb7edd6cede2b6f08274126911c
Henrietteph <115145767+Henrietteph@users.noreply.github.com>
2025-09-19 20:59:39 +0200

    Update hyttetur1.py

--- hyttetur1.py
+++ hyttetur1.py
@@ -1,4 +1,5 @@
-import gspread from oauth2client.service_account import ServiceAccountCredentials
+import gspread 
+from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
 import streamlit as st
 
@@ -67,5 +68,6 @@ else:
 new_name = st.text_input("Legg til deltaker")
 if st.button("Legg til"):
     st.session_state.players[new_name] = 0
+
 
 
