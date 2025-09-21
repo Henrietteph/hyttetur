c2a865f4eee6c878a5956e3c8a394cd079b7ff1f
Henrietteph <115145767+Henrietteph@users.noreply.github.com>
2025-09-21 11:13:42 +0200

    Update hyttetur1.py

--- hyttetur1.py
+++ hyttetur1.py
@@ -68,7 +68,9 @@ st.title("🏆 Poengtavle")
 
 # Last inn spillere
 players = load_players_records(sheet)
-st.write(players{"Anette"})
+player = players.row[1]
+st.write(players)
+st.write(player)
 
 # Eksempel: hent Kari hvis hun finnes
 if "Henriette" in players:
@@ -99,6 +101,7 @@ else:
 new_name = st.text_input("Legg til deltaker")
 if st.button("Legg til"):
     st.session_state.players[new_name] = 0
+
 
 
 
